163c28488e701d0b41ff98d9d53c688f782d0e2f
asimrepo <masim015@yahoo.com>
2017-11-05 16:57:23 +0500

    Added copyfile.py to repo

---
+++ copyfile.py
@@ -0,0 +1,33 @@
+import shutil
+import os
+#os.chdir('D:\BE_DB\BE - Backup - 31-05-2017')
+#print(os.getcwd())
+for root,dirs,files in os.walk("D:\BE_DB\BE - Backup - 31-05-2017"):
+    for filename in files:
+        print(filename)
+#os.path.listOfFiles()
+
+
+ #for(int i=0;i++;len(file_list)):
+        
+        if(file_list[0] | file_list[2] | file_list[3]):
+            if(os.path.isfile(os.file_list[0]) | os.path.isfile(file_list[2]) | os.path.isfile(file_list[3])):
+                continue
+            elif(file_list[1] | file_list[4] | file_list[5]):
+                continue
+            else:
+                shutil.copy(os.path.join(directory,i),"D:\\BE_DB\\")
+
+
+
+if((idx==1) | (idx==4) | (idx==5)):
+            continue
+        else:
+            shutil.copy(os.path.join(dir,str(value)),dir1)
+        
+        #print(c)
+       # c=c+1
+        count=len(file_list)
+                
+
+
